b0d95023f77396515206230473fdb93fc7151cdd
gholk <gholk@disroot.org>
2023-12-05 22:56:52 +0800

    Add a textarea to edit the text of qrcode
    The textarea is minimize when unfocused.
    The qrcode will get updated when input events fire,
    and the event is throttled to 1s.

--- src/qrCodeGenerator.ts
+++ src/qrCodeGenerator.ts
@@ -1,6 +1,6 @@
 import QRCode from "../vendor/qrcode/qrcode"
 import * as Logging from "@src/lib/logging"
-import {browserBg, activeTab} from "./lib/webext"
+import { browserBg, activeTab } from "./lib/webext"
 
 const logger = new Logging.Logger("qrcode-display")
 
@@ -11,18 +11,7 @@ function displayError() {
     errorDisplay.innerHTML = "Unable to generate QR code for the given data"
 }
 
-function setUpPage() {
-    const imgElem: HTMLImageElement =
-        document.querySelector("div#qr-canvas img")
-
-    const url = new URL(window.location.href)
-    let data = url.searchParams.get("data")
-    const timeout = parseInt(url.searchParams.get("timeout"), 10)
-    data = decodeURIComponent(atob(data))
-    const opts = {
-        scale: 10,
-    }
-
+function displayQRCode(data, imgElem, opts = { scale: 10 }) {
     QRCode.toDataURL(data, opts, (error: Error, url: string) => {
         if (error) {
             logger.error(error)
@@ -31,16 +20,49 @@ function setUpPage() {
             imgElem.src = url
         }
     })
+}
+
+function setUpPage() {
+    const imgElem: HTMLImageElement =
+        document.querySelector("div#qr-canvas img")
+
+    const url = new URL(window.location.href)
+    let data = url.searchParams.get("data")
+    const timeout = parseInt(url.searchParams.get("timeout"), 10)
+    data = decodeURIComponent(atob(data))
+    displayQRCode(data, imgElem)
 
     if (timeout && timeout > 0) {
-        setTimeout(function() {
-            activeTab().then((tabInfo) => {
-                browserBg.tabs.remove(tabInfo.id)
-            }).catch((error) => {
-                logger.error("Unable to close tab" + error)
-            })
+        setTimeout(function () {
+            activeTab()
+                .then(tabInfo => {
+                    browserBg.tabs.remove(tabInfo.id)
+                })
+                .catch(error => {
+                    logger.error("Unable to close tab" + error)
+                })
         }, timeout * 1000)
+    }
+
+    const textarea = document.getElementsByName(
+        "data",
+    )[0] as HTMLTextAreaElement
+    textarea.value = data
+    textarea.addEventListener(
+        "input",
+        throttle(event => displayQRCode(event.target.value, imgElem), 1000),
+    )
+}
+
+function throttle(fn, ms) {
+    let timeLast
+    return (...args) => {
+        const time = (timeLast = Date.now())
+        setTimeout(() => {
+            if (time == timeLast) fn(...args)
+        }, ms)
     }
 }
 
+window["QRCode"] = QRCode
 window.addEventListener("load", setUpPage)
